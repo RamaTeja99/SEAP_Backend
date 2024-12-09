51ed7cf31feade4d9e63e74138f52f2f2275399b
RamaTeja99 <144083564+RamaTeja99@users.noreply.github.com>
2024-12-09 17:31:19 +0530

    Update UserController.java

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://seap.netlify.app", allowedHeaders = "*")
 public class UserController {
 
     @Autowired
